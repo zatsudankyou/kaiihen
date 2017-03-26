b9cdd78bbc686ffe5ed61a0cd8c49d9c8833f973
karaage1219 <karaage19991219@gmail.com>
2017-03-26 21:51:08 +0900

    わーい！ついにやったぞ！ 画像表示問題が解決

--- LoadImg.h
+++ LoadImg.h
@@ -1,30 +1,26 @@
 //二重include防止
 #ifndef DFF_LOAD_IMG_H
 
-#define DFF_LOAD_IMG_H
-
 //宣言
 void LoadImg();
 void M_MenuLoadImg();
-void GameBackLoadImg();
+void GameBackLoadImg(); 
 void DrawPlayer();
 void DrawM_Menu();
 
-//ロード画像の宣言
-extern int PlayerImg;
-extern int AmmoImg;
-extern int BackImg;
-extern int LogoImg;
-extern int SelectMainMenuImg0;
-extern int SelectMainMenuImg1;
-extern int SelectMainMenuImg2;
-extern int SelectMainMenuImg3;
-extern int SelectMainMenuImg0N;
-extern int SelectMainMenuImg1N;
-extern int SelectMainMenuImg2N;
-extern int SelectMainMenuImg3N;
-extern int AboutImg;
-extern int WeponImg;
-extern int MBackImg;
-
-#endif
+//ロード画像のプロトタイプ宣言
+int PlayerImg;
+int AmmoImg;
+int BackImg;
+int LogoImg;
+int SelectMainMenuImg0;
+int SelectMainMenuImg1;
+int SelectMainMenuImg2;
+int SelectMainMenuImg3;
+int SelectMainMenuImg0N;
+int SelectMainMenuImg1N;
+int SelectMainMenuImg2N;
+int SelectMainMenuImg3N;
+int AboutImg;
+int WeponImg;
+int MBackImg;
